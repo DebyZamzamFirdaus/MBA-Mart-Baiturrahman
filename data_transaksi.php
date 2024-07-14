9e8876d062cf99b85f2154254df91abba2d5b6ae
FajarAziziBM <fajaraziziboangmanalu97902@gmail.com>
2024-07-14 23:24:07 +0700

    fix(tambah data) berhasi, saya merefaktor database.php

--- data_transaksi.php
+++ data_transaksi.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['apriori_id'])) {
 
 include_once "database.php";
 include_once "fungsi.php";
+include_once "helpers.php";
 include_once "import/excel_reader2.php";
 ?>
 
@@ -13,87 +14,99 @@ include_once "import/excel_reader2.php";
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-money-bill-wave"></i> Data Transaksi</h1>
 
-	<div class="text-right">
-	<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalupload">
-	<i class="fa fa-upload"></i> Upload Data 
-	</button>
-	<button type="button" class="btn btn-success" data-toggle="modal" data-target="#modaladd">
-	<i class="fa fa-plus"></i> Tambah Data 
-	</button>
-	</div>
+    <div class="text-right">
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalupload">
+            <i class="fa fa-upload"></i> Upload Data
+        </button>
+        <button type="button" class="btn btn-success" data-toggle="modal" data-target="#modaladd">
+            <i class="fa fa-plus"></i> Tambah Data
+        </button>
+    </div>
 </div>
 
 <?php
 $db_object = new database();
 
 $pesan_error = $pesan_success = "";
-if(isset($_GET['pesan_error'])){
+if (isset($_GET['pesan_error'])) {
     $pesan_error = $_GET['pesan_error'];
 }
-if(isset($_GET['pesan_success'])){
+if (isset($_GET['pesan_success'])) {
     $pesan_success = $_GET['pesan_success'];
 }
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $data = new Spreadsheet_Excel_Reader($_FILES['file_data_transaksi']['tmp_name']);
 
-        $baris = $data->rowcount($sheet_index=0);
-        $column = $data->colcount($sheet_index=0);
+    $baris = $data->rowcount($sheet_index = 0);
+    $column = $data->colcount($sheet_index = 0);
 
-        for ($i=2; $i<=$baris; $i++) {
-            for($c=1; $c<=$column; $c++){
-                $value[$c] = $data->val($i, $c);
-            }
-
-                    $table = "transaksi";
-                    // $produkIn = get_produk_to_in($temp_produk);
-                    $temp_date = format_date($value[1]);
-                    $produkIn = $value[2];
-                    
-                    //mencegah ada jarak spasi
-                    $produkIn = str_replace(" ,", ",", $produkIn);
-                    $produkIn = str_replace("  ,", ",", $produkIn);
-                    $produkIn = str_replace("   ,", ",", $produkIn);
-                    $produkIn = str_replace("    ,", ",", $produkIn);
-                    $produkIn = str_replace(", ", ",", $produkIn);
-                    $produkIn = str_replace(",  ", ",", $produkIn);
-                    $produkIn = str_replace(",   ", ",", $produkIn);
-                    $produkIn = str_replace(",    ", ",", $produkIn);
-                    $sql = "INSERT INTO transaksi (transaction_date, produk) VALUES ";
-                    $value_in = array();
-                    $sql .= " ('$temp_date', '$produkIn')";
-                    $db_object->db_query($sql);
+    for ($i = 2; $i <= $baris; $i++) {
+        for ($c = 1; $c <= $column; $c++) {
+            $value[$c] = $data->val($i, $c);
         }
-        ?>
-        <script> location.replace("?menu=data_transaksi&pesan_success=Data berhasil disimpan"); </script>
-        <?php
+
+        $table = "transaksi";
+        // $produkIn = get_produk_to_in($temp_produk);
+        $temp_date = format_date($value[1]);
+        $produkIn = $value[2];
+
+        //mencegah ada jarak spasi
+        $produkIn = str_replace(" ,", ",", $produkIn);
+        $produkIn = str_replace("  ,", ",", $produkIn);
+        $produkIn = str_replace("   ,", ",", $produkIn);
+        $produkIn = str_replace("    ,", ",", $produkIn);
+        $produkIn = str_replace(", ", ",", $produkIn);
+        $produkIn = str_replace(",  ", ",", $produkIn);
+        $produkIn = str_replace(",   ", ",", $produkIn);
+        $produkIn = str_replace(",    ", ",", $produkIn);
+        $sql = "INSERT INTO transaksi (transaction_date, produk) VALUES ";
+        $value_in = array();
+        $sql .= " ('$temp_date', '$produkIn')";
+        $db_object->db_query($sql);
+    }
+?>
+    <script>
+        location.replace("?menu=data_transaksi&pesan_success=Data berhasil disimpan");
+    </script>
+<?php
 }
 
 if (isset($_POST['simpan'])) {
-  $transaction_date = $_POST['transaction_date'];
-  $produk = $_POST['produk'];
-  $sql = "INSERT INTO transaksi (id,transaction_date,produk) VALUES ('','$transaction_date','$produk')";
-  $db_object->db_query($sql);
-  if ($sql) {
-    echo "<script> location.replace('?menu=data_transaksi&pesan_success=Data berhasil disimpan'); </script>";
-  } else {
-    echo "<script> location.replace('?menu=data_transaksi&pesan_success=Data gagal disimpan'); </script>";
-  }
+    $transaction_date = $_POST['transaction_date'];
+    $produk = $_POST['produk'];
+
+    // Query untuk memasukkan data baru ke dalam tabel 'transaksi'
+    $sql = "INSERT INTO transaksi (transaction_date, produk) VALUES ('$transaction_date', '$produk')";
+
+    // Eksekusi query menggunakan method db_query dari object $db_object
+    $result = $db_object->db_query($sql);
+
+    if (!$result) {
+        // Jika query gagal, tangkap pesan kesalahan dari koneksi
+        $error_message = $db_object->db_error();
+        echo "<script> location.replace('?menu=data_transaksi&pesan_error=Data gagal disimpan: $error_message'); </script>";
+    } else {
+        // Jika query berhasil
+        echo "<script> location.replace('?menu=data_transaksi&pesan_success=Data berhasil disimpan'); </script>";
+    }
 }
 
 
-if(isset($_POST['delete'])){
+if (isset($_POST['delete'])) {
     $sql = "TRUNCATE transaksi";
     $db_object->db_query($sql);
-    ?>
-        <script> location.replace("?menu=data_transaksi&pesan_success=Data transaksi berhasil dihapus"); </script>
-        <?php
+?>
+    <script>
+        location.replace("?menu=data_transaksi&pesan_success=Data transaksi berhasil dihapus");
+    </script>
+<?php
 }
 
 if (isset($_GET['hapus'])) {
     $id = $_GET['hapus'];
     $sql = "DELETE FROM transaksi WHERE id='$id'";
-	$db_object->db_query($sql);
+    $db_object->db_query($sql);
     if ($sql) {
         echo "<script> location.replace('?menu=data_transaksi&pesan_success=Data transaksi berhasil dihapus'); </script>";
     }
@@ -103,8 +116,8 @@ $sql = "SELECT
         *
         FROM
          transaksi";
-$query=$db_object->db_query($sql);
-?>     
+$query = $db_object->db_query($sql);
+?>
 
 
 <div class="modal fade" id="modalupload" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
@@ -148,21 +161,21 @@ $query=$db_object->db_query($sql);
                     </button>
                 </div>
                 <div class="modal-body">
-                    
-                <div class="col-12">
-                    <label for="" class="font-weight-bold">Tanggal</label>
-                    <div class="form-group">
-                        <input type="date" name="transaction_date" class="form-control" required="" />
+
+                    <div class="col-12">
+                        <label for="" class="font-weight-bold">Tanggal</label>
+                        <div class="form-group">
+                            <input type="date" name="transaction_date" class="form-control" required="" />
+                        </div>
                     </div>
-                </div>
-				
-				<div class="col-12">
-                    <label for="" class="font-weight-bold">Produk</label>
-                    <div class="form-group">
-						<textarea class="form-control" name="produk" rows="3" required=""></textarea>
+
+                    <div class="col-12">
+                        <label for="" class="font-weight-bold">Produk</label>
+                        <div class="form-group">
+                            <textarea class="form-control" name="produk" rows="3" required=""></textarea>
+                        </div>
                     </div>
-                </div>
-				
+
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-warning" data-dismiss="modal"><i class="fas fa-fw fa-times"></i> Close</button>
@@ -178,65 +191,66 @@ $query=$db_object->db_query($sql);
     <!-- /.card-header -->
     <div class="card-header py-3 d-sm-flex align-items-center justify-content-between">
         <h6 class="m-0 font-weight-bold text-success"><i class="fa fa-table"></i> Daftar Data Transaksi</h6>
-		
-		<form method="post" enctype="multipart/form-data" action="">
-			<button name="delete" type="submit" class="btn btn-app btn-danger btn-sm" onclick="return confirm('Are you sure?')"><i class="fas fa-fw fa-trash"></i> Delete All Data</button>
-		</form>
+
+        <form method="post" enctype="multipart/form-data" action="">
+            <button name="delete" type="submit" class="btn btn-app btn-danger btn-sm" onclick="return confirm('Are you sure?')"><i class="fas fa-fw fa-trash"></i> Delete All Data</button>
+        </form>
     </div>
 
     <div class="card-body">
-        
 
-            <?php
-            if (!empty($pesan_error)) {
-                display_error($pesan_error);
-            }
-            if (!empty($pesan_success)) {
-                display_success($pesan_success);
-            }
-            ?>
-            <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+
+        <?php
+        if (!empty($pesan_error)) {
+            display_error($pesan_error);
+        }
+        if (!empty($pesan_success)) {
+            display_success($pesan_success);
+        }
+        ?>
+        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
             <thead class="bg-success text-white">
                 <tr align="center">
-                <th>No</th>
-                <th>Tanggal</th>
-                <th>Produk</th>
-				<th width="15%">Aksi</th>
+                    <th>No</th>
+                    <th>Tanggal</th>
+                    <th>Produk</th>
+                    <th width="15%">Aksi</th>
                 </tr>
-			</thead>
-                <?php
-                    $no=1;
-                    while($row=$db_object->db_fetch_array($query)){
-				?>
-                        <tr>
-							<td class='text-center'> <?= $no ?></td>
-                            <td class='text-center'> <?= format_date2($row['transaction_date']) ?></td>
-                            <td> <?= $row['produk'] ?></td>
-							<td class='text-center'>
-							<div class='btn-group' role='group'>
-								<a href="?menu=edit_transaksi&id=<?= $row['id'] ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
-								<a href="?menu=data_transaksi&hapus=<?= $row['id'] ?>" class="btn btn-danger btn-sm" onclick="return confirm ('Apakah anda yakin untuk meghapus data ini')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-							</div>
-							</td>
-                        </tr>
-                    
-					<?php
-						$no++;
-                    }
-                    ?>
-            </table>
-            </div>
-            </div>
+            </thead>
+            <?php
+            $no = 1;
+            while ($row = $db_object->db_fetch_array($query)) {
+            ?>
+                <tr>
+                    <td class='text-center'> <?= $no ?></td>
+                    <td class='text-center'> <?= format_date2($row['transaction_date']) ?></td>
+                    <td> <?= $row['produk'] ?></td>
+                    <td class='text-center'>
+                        <div class='btn-group' role='group'>
+                            <a href="?menu=edit_transaksi&id=<?= $row['id'] ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+                            <a href="?menu=data_transaksi&hapus=<?= $row['id'] ?>" class="btn btn-danger btn-sm" onclick="return confirm ('Apakah anda yakin untuk meghapus data ini')" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+                        </div>
+                    </td>
+                </tr>
+
+            <?php
+                $no++;
+            }
+            ?>
+        </table>
+    </div>
+</div>
 
 
 <?php
-function get_produk_to_in($produk){
+function get_produk_to_in($produk)
+{
     $ex = explode(",", $produk);
     //$temp = "";
-    for ($i=0; $i < count($ex); $i++) { 
+    for ($i = 0; $i < count($ex); $i++) {
 
         $jml_key = array_keys($ex, $ex[$i]);
-        if(count($jml_key)>1){
+        if (count($jml_key) > 1) {
             unset($ex[$i]);
         }
 
@@ -246,4 +260,3 @@ function get_produk_to_in($produk){
 }
 
 ?>
-  
--- database.php
+++ database.php
@@ -1,5 +1,6 @@
 <?php
-class database {
+class database
+{
 
     private $servername;
     private $user_db;
@@ -8,15 +9,20 @@ class database {
     private $koneksi;
 
     /**
-     * fungsi construktor awal dan koneksi ke database;
-     * set variable class $this->conn;
+     * Constructor untuk koneksi ke database
+     * Menginisialisasi properti koneksi dari hasil panggilan connect_db()
      */
-    function __construct() {
-        $this->load_conf_db();
-        return $this->connect_db();
+    function __construct()
+    {
+        $this->load_conf_db(); // Memuat konfigurasi koneksi dari file eksternal
+        $this->connect_db(); // Menghubungkan ke database menggunakan konfigurasi yang dimuat
     }
 
-    function load_conf_db() {
+    /**
+     * Memuat konfigurasi database dari file koneksi.php jika ada
+     */
+    function load_conf_db()
+    {
         $path = dirname(__FILE__) . '/koneksi.php';
         if (file_exists($path)) {
             $conf = include $path;
@@ -28,246 +34,206 @@ class database {
     }
 
     /**
-     * connection database
-     * @return type
+     * Menghubungkan ke database menggunakan konfigurasi yang telah dimuat
+     * @return mysqli|null
      */
-    public function connect_db() {
+    public function connect_db()
+    {
         $this->koneksi = mysqli_connect($this->servername, $this->user_db, $this->password_db, $this->database);
-        return $this->koneksi;
+        if (!$this->koneksi) {
+            die("Koneksi database gagal: " . mysqli_connect_error());
+        }
     }
 
     /**
-     * execute query mysql_query
-     * @param string $sql
-     * @return type
+     * Eksekusi query SQL
+     * @param string $sql Query SQL yang akan dieksekusi
+     * @return bool|mysqli_result Hasil eksekusi query
      */
-    function db_query($sql) {
-        $conn = $this->koneksi;
-        return mysqli_query($conn, $sql);
+    function db_query($sql)
+    {
+        return mysqli_query($this->koneksi, $sql);
     }
 
     /**
-     * mysql_error
-     * @return type
+     * Mendapatkan pesan kesalahan dari operasi database terakhir
+     * @param $result Hasil dari operasi database
+     * @return string Pesan kesalahan MySQL
      */
-    function db_error($result) {
-        $conn = $this->koneksi;
-        return mysqli_error($conn);
+    function db_error($result)
+    {
+        return mysqli_error($this->koneksi);
     }
 
     /**
-     * mysql_fetch_array
-     * @param type $result
-     * @return type
+     * Mendapatkan array asosiatif dari hasil query SELECT
+     * @param $result Hasil dari query SELECT
+     * @return array|null Hasil dalam bentuk array asosiatif
      */
-    function db_fetch_array($result) {
-        return mysqli_fetch_array($result);
+    function db_fetch_array($result)
+    {
+        return mysqli_fetch_array($result, MYSQLI_ASSOC);
     }
 
     /**
-     * mysql_num_rows
-     * @param type $result
-     * @return type
+     * Mendapatkan jumlah baris dari hasil query SELECT
+     * @param $result Hasil dari query SELECT
+     * @return int Jumlah baris
      */
-    function db_num_rows($result) {
+    function db_num_rows($result)
+    {
         return mysqli_num_rows($result);
     }
 
     /**
-     * mysql_insert_id
-     * @return type
+     * Mendapatkan ID yang di-generate oleh operasi INSERT terakhir
+     * @return int ID yang di-generate oleh operasi INSERT
      */
-    function db_insert_id() {
-        $conn = $this->koneksi;
-        return mysqli_insert_id($conn);
-    }
-    
-
-//==============================================================================
-    /**
-     * Insert Data within table by accepting TableName and Table column => Data as associative array
-     * contoh $val_cols = array("Name"=>'ido',"Age"=>23);
-     * @param string $table
-     * @param array $val_cols 
-     * @return type hasil query (db_query)
-     */
-    function insert_record($table, array $val_cols) {
-        $field = implode("`, `", array_keys($val_cols));
-        $i = 0;
-        foreach ($val_cols as $key => $value) {
-            $StValue[$i] = "'" . $value . "'";
-            $i++;
-        }
-        $StValues = implode(", ", $StValue);
-
-        $sql = "INSERT INTO $table (`$field`) VALUES ($StValues)";
-        
-        $result = $this->db_query($sql);
-        return $result;
+    function db_insert_id()
+    {
+        return mysqli_insert_id($this->koneksi);
     }
 
+    //==============================================================================
+
     /**
-     * Delete data form table; Accepting Table Name and Keys=>Values as associative array
-     * contoh $val_cols = array("id"=>4,"Name"=>'ido');
-     * @param string $table
-     * @param array $val_cols where condition implode by AND
-     * @return type hasil query (db_query)
+     * Memasukkan record baru ke dalam tabel
+     * @param string $table Nama tabel
+     * @param array $val_cols Array asosiatif kolom => nilai
+     * @return bool Hasil eksekusi query
      */
-    function delete_record($table, array $val_cols) {
-        //Append each element of val_cols associative array 
-        $i = 0;
-        foreach ($val_cols as $key => $value) {
-            $exp[$i] = $key . " = '" . $value . "'";
-            $i++;
-        }
-        $Stexp = implode(" AND ", $exp);
-
-        $sql = "DELETE FROM $table WHERE $Stexp ";
-
+    function insert_record($table, array $val_cols)
+    {
+        $fields = implode("`, `", array_keys($val_cols));
+        $values = implode("', '", array_values($val_cols));
+        $sql = "INSERT INTO $table (`$fields`) VALUES ('$values')";
         return $this->db_query($sql);
     }
 
     /**
-     * Update data within table; Accepting Table Name and Keys=>Values as associative array
-     * contoh $set_val_cols = array("Name"=>'Ido',"Nilai"=>98.9);
-     * contoh $cod_val_cols = array("id"=>'23');
-     * @param type $table
-     * @param array $set_val_cols set update
-     * @param array $cod_val_cols where condition implode by AND
-     * @return type
+     * Menghapus record dari tabel berdasarkan kondisi
+     * @param string $table Nama tabel
+     * @param array $val_cols Kondisi sebagai array kolom => nilai
+     * @return bool Hasil eksekusi query
      */
-    function update_record($table, array $set_val_cols, array $cod_val_cols) {
-        //append set_val_cols associative array elements 
-        $i = 0;
-        foreach ($set_val_cols as $key => $value) {
-            $set[$i] = $key . " = '" . $value . "'";
-            $i++;
+    function delete_record($table, array $val_cols)
+    {
+        $conditions = array();
+        foreach ($val_cols as $key => $value) {
+            $conditions[] = "$key = '$value'";
         }
-        $Stset = implode(", ", $set);
+        $where_clause = implode(" AND ", $conditions);
+        $sql = "DELETE FROM $table WHERE $where_clause";
+        return $this->db_query($sql);
+    }
 
-        //append cod_val_cols associative array elements
-        $i = 0;
+    /**
+     * Memperbarui record dalam tabel berdasarkan kondisi
+     * @param string $table Nama tabel
+     * @param array $set_val_cols Array asosiatif untuk kolom yang akan di-update
+     * @param array $cod_val_cols Kondisi sebagai array kolom => nilai
+     * @return bool Hasil eksekusi query
+     */
+    function update_record($table, array $set_val_cols, array $cod_val_cols)
+    {
+        $set_clause = implode(", ", array_map(function ($key, $value) {
+            return "$key = '$value'";
+        }, array_keys($set_val_cols), array_values($set_val_cols)));
+
+        $conditions = array();
         foreach ($cod_val_cols as $key => $value) {
-            $cod[$i] = $key . " = '" . $value . "'";
-            $i++;
+            $conditions[] = "$key = '$value'";
         }
-        $Stcod = implode(" AND ", $cod);
+        $where_clause = implode(" AND ", $conditions);
 
-        $sql = "UPDATE $table SET $Stset WHERE $Stcod";
-
+        $sql = "UPDATE $table SET $set_clause WHERE $where_clause";
         return $this->db_query($sql);
     }
 
     //--------------------------------------------------------------------------
 
     /**
-     * menghitung jumlah data (count)
-     * @param type $table
-     * @param type $field
-     * @param type $where default=null, diisi dengan kolom dan isi nya contoh warna='blue'
-     * @return int hasil count (db_fetch_array)
+     * Menghitung jumlah data dalam tabel
+     * @param string $table Nama tabel
+     * @param string $field Kolom yang dihitung
+     * @param string $where Kondisi WHERE (opsional)
+     * @return array|null Hasil dalam bentuk array asosiatif
      */
-    function count_data($table, $field, $where = null) {
-        $sql = "SELECT COUNT($field) FROM $table ";
-
-        if ($where != null || $where != '') {
-            $sql .= " WHERE $where ";
-        }
-
-        $result = $this->db_query($sql);
-        return $row = $this->db_fetch_array($result);
-    }
-
-    /**
-     * ambil data table semua kolom return bisa db_query/db_fetch_array
-     * bisa di limit
-     * @param type $table
-     * @param type $where default=null
-     * @param type $fetch default=false
-     * @param type $limit default=false
-     * @param type $limit_posisi default=0
-     * @param type $limit_batas default=0
-     * @param type $sort default='', order by...
-     * @return fetch/db_query
-     */
-    function display_table_all_column($table, $where = null, $fetch = false, $limit = false, $limit_posisi = 0, $limit_batas = 0, $sort = '') {
-        $sql = "SELECT * FROM $table ";
-
-        if ($where != null || $where != '') {
+    function count_data($table, $field, $where = null)
+    {
+        $sql = "SELECT COUNT($field) AS count FROM $table";
+        if (!empty($where)) {
             $sql .= " WHERE $where";
         }
+        $result = $this->db_query($sql);
+        return $this->db_fetch_array($result);
+    }
 
+    /**
+     * Menampilkan semua kolom dari tabel
+     * @param string $table Nama tabel
+     * @param string $where Kondisi WHERE (opsional)
+     * @param bool $fetch Mengembalikan hasil fetch_array atau hanya query (opsional, default false)
+     * @param bool $limit Menggunakan LIMIT (opsional, default false)
+     * @param int $limit_offset Posisi awal LIMIT (opsional, default 0)
+     * @param int $limit_count Jumlah batasan LIMIT (opsional, default 0)
+     * @param string $sort Klausa ORDER BY (opsional, default '')
+     * @return array|null|bool Hasil fetch_array atau query (tergantung pada $fetch)
+     */
+    function display_table_all_column($table, $where = null, $fetch = false, $limit = false, $limit_offset = 0, $limit_count = 0, $sort = '')
+    {
+        $sql = "SELECT * FROM $table";
+        if (!empty($where)) {
+            $sql .= " WHERE $where";
+        }
         if (!empty($sort)) {
-            $sql .= " ORDER BY " . $sort;
+            $sql .= " ORDER BY $sort";
         }
-
         if ($limit) {
-            $sql .= " LIMIT $limit_posisi , $limit_batas ";
+            $sql .= " LIMIT $limit_offset, $limit_count";
         }
-
-        if ($fetch) {
-            $result = $this->db_query($sql);
-            return $row = $this->db_fetch_array($result);
-        } else {
-            return $result = $this->db_query($sql);
-        }
+        return ($fetch) ? $this->db_fetch_array($this->db_query($sql)) : $this->db_query($sql);
     }
 
     /**
-     * mencari sesuatu dalam sebuah table
-     * 
-     * @param string $table 
-     * @param array string $find_column default = array()
-     * @param string $where default=''
-     * @return array db_fetch_array
+     * Mencari nilai dalam tabel berdasarkan kolom
+     * @param string $table Nama tabel
+     * @param array|string $find_column Kolom yang dicari
+     * @param string $where Kondisi WHERE (opsional)
+     * @return array|null Hasil dalam bentuk array asosiatif
      */
-    function find_in_table($table, $find_column = array(), $where = '') {
-        $sql = "SELECT ";
-        $column = '';
-        if (!is_array($find_column)) {
-            $column = $find_column;
-        } else {
-            $column = implode(",", $find_column);
-        }
-        $sql .= $column . " FROM " . $table . " " . $where;
-
+    function find_in_table($table, $find_column = array(), $where = '')
+    {
+        $columns = (is_array($find_column)) ? implode(", ", $find_column) : $find_column;
+        $sql = "SELECT $columns FROM $table $where";
         $result = $this->db_query($sql);
-        $rows = $this->db_fetch_array($result);
-
-        return $rows;
+        return $this->db_fetch_array($result);
     }
 
     /**
-     * cek value data pada suatu table
-     * @param string $table
-     * @param string $field
-     * @param string $value
-     * @return boolean true jika ada , false jika tidak ada
-     * */
-    function cek_data_is_in_table($table, $field, $value) {
-        $sql = "SELECT COUNT(" . $field . ") FROM " . $table . " WHERE " . $field . " = '" . $value . "'";
-        $result = $this->db_query($sql);
-        $num = $this->db_fetch_array($result);
-
-        if ($num[0] > 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /**
-     * ambil data login user
-     * @param type $id_login
-     * @return type
+     * Memeriksa apakah data ada dalam tabel
+     * @param string $table Nama tabel
+     * @param string $field Kolom yang diperiksa
+     * @param string $value Nilai yang dicari dalam kolom
+     * @return bool True jika data ada, False jika tidak ada
      */
-    function get_login_by_id($id_login){
-        $sql = "SELECT * FROM login WHERE id_login = ".$id_login;
+    function cek_data_is_in_table($table, $field, $value)
+    {
+        $sql = "SELECT COUNT($field) AS count FROM $table WHERE $field = '$value'";
         $result = $this->db_query($sql);
         $row = $this->db_fetch_array($result);
-        return $row;
+        return ($row['count'] > 0);
     }
 
+    /**
+     * Mendapatkan data login berdasarkan ID login
+     * @param int $id_login ID login yang dicari
+     * @return array|null Data login dalam bentuk array asosiatif
+     */
+    function get_login_by_id($id_login)
+    {
+        $sql = "SELECT * FROM login WHERE id_login = $id_login";
+        $result = $this->db_query($sql);
+        return $this->db_fetch_array($result);
+    }
 }
-
-?>
