ceca0c24d60a7e4072e00326c2c7ac8319c11153
DebyZamzamFirdaus <debyfirda29@gmail.com>
2024-07-14 12:07:19 +0700

    Mart Baiturrahman

---
+++ forbidden.php
@@ -0,0 +1,6 @@
+<div class="text-center">
+  <div class="error mx-auto" data-text="404">403</div>
+  <p class="lead text-gray-800 mb-5">Forbidden</p>
+  <p class="text-gray-500 mb-0">Sepertinya Anda menemukan kesalahan pada matriks...</p>
+  <a href="index.php">&larr; Kembali ke Dashboard</a>
+</div>
---
+++ home.php
@@ -0,0 +1,72 @@
+<div class="mb-4">
+
+  <!-- Page Heading -->
+  <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <h1 class="h3 mb-0 text-gray-800"><i class="fas fa-fw fa-home"></i> MART BAITURRAHMAN LHOKSEUMAWE</h1>
+  </div>
+
+  <!-- Content Row -->
+  <div class="row">
+
+    <div class="col-xl-4 col-md-6 mb-4">
+      <div class="card border-left-danger shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="index.php?menu=data_transaksi" class="text-secondary text-decoration-none">Data Transaksi</a></div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-money-bill-wave fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xl-4 col-md-6 mb-4">
+      <div class="card border-left-warning shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="index.php?menu=proses_apriori" class="text-secondary text-decoration-none">Proses Apriori</a></div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-sync fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xl-4 col-md-6 mb-4">
+      <div class="card border-left-success shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="index.php?menu=proses_cmar" class="text-secondary text-decoration-none">Proses CMAR</a></div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-sync fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-xl-4 col-md-6 mb-4">
+      <div class="card border-left-secondary shadow h-100 py-2">
+        <div class="card-body">
+          <div class="row no-gutters align-items-center">
+            <div class="col mr-2">
+              <div class="h5 mb-0 font-weight-bold text-gray-800"><a href="index.php?menu=hasil" class="text-secondary text-decoration-none">Data Hasil</a></div>
+            </div>
+            <div class="col-auto">
+              <i class="fas fa-coins fa-2x text-gray-300"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</div>
